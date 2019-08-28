6edca8b82b1f52d4400cb0c4e18fb0a911a53986
alvarogabriel-ramirez <aramirez-2018025@kinal.edu.gt>
2019-08-28 14:32:40 -0600

    Creacion de proyecto

---
+++ Envio.php
@@ -0,0 +1,20 @@
+<?php
+
+	
+	$idusuario = $_POST ['usuario'];
+	echo $idusuario;
+	
+	$nombre = $_POST ['nombre'];
+	echo $nombre;
+	
+	$apellido = $_POST ['apellido'];
+	echo $apellido;
+	
+	$email = $_POST ['email'];
+	echo $email;
+	
+	$pass = $_POST ['pass'];
+	echo $pass;
+	
+
+?>
